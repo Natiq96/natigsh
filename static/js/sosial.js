edf399380f79592fc228d75d52aa6cb4d2044251
NatigShukurov <thntg0906@gmail.com>
2025-06-22 02:34:35 +0400

    natig_96_4

---
+++ static/js/sosial.js
@@ -0,0 +1,20 @@
+const fullHeight = document.documentElement.scrollHeight;
+const solidbar = document.getElementById("solidbar")
+solidbar.style.height = fullHeight
+
+const menu = document.getElementById("menu")
+const menuIcon = document.getElementById("menuIcon")
+menu.addEventListener("click",function(e){
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    })
+    if (menuIcon.name === "close-circle-outline"){
+        solidbar.style.display = "none"
+        menuIcon.name = "menu-sharp"
+    }
+    else{
+        solidbar.style.display = "flex"
+        menuIcon.name = "close-circle-outline"
+    }
+})
